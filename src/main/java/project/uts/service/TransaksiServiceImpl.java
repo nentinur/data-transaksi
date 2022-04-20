5c8272a5939e28431ec6515fc13e662f7ef3d51f
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2022-04-21 00:16:37 +0700

    Menambahkan TransaksiServiceImpl.java

---
+++ src/main/java/project/uts/service/TransaksiServiceImpl.java
@@ -0,0 +1,39 @@
+package project.uts.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import project.uts.entity.Transaksi;
+import project.uts.repository.TransaksiRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TransaksiServiceImpl implements TransaksiService {
+    private final TransaksiRepository transaksiRepository;
+
+    @Autowired
+    public TransaksiServiceImpl(TransaksiRepository studentRepository) {
+        this.transaksiRepository = transaksiRepository;
+    }
+
+    @Override
+    public List<Transaksi> getAllTransaksi() {
+        return transaksiRepository.findAll();
+    }
+
+    @Override
+    public Optional<Transaksi> findById(Long id) {
+        return transaksiRepository.findById(id);
+    }
+
+    @Override
+    public Transaksi save(Transaksi std) {
+        return transaksiRepository.save(std);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        transaksiRepository.deleteById(id);
+    }
+}
