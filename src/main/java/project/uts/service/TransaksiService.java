c0e0aaf05cb8d72c3bf7b9916344a289c15b176b
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2022-04-20 23:35:49 +0700

    Menambahkan TransaksiService.java

---
+++ src/main/java/project/uts/service/TransaksiService.java
@@ -0,0 +1,14 @@
+package project.uts.service;
+
+import project.uts.entity.Transaksi;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TransaksiService {
+    List<Transaksi> getAllTransaksi();
+    Optional<Transaksi> findById();
+    Transaksi save(Transaksi std);
+    void deleteById(Long id);
+
+}
