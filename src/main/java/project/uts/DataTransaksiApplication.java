fa1d7dc778017b42d9e937577b9dcefaa6ee2275
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2022-04-20 19:48:29 +0700

    Initial commit

---
+++ src/main/java/project/uts/DataTransaksiApplication.java
@@ -0,0 +1,13 @@
+package project.uts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTransaksiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataTransaksiApplication.class, args);
+	}
+
+}
